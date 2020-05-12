bef6882937331b391ea0e60720782528187a30db
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-05-12 23:06:05 +0530

    initial commit

---
+++ functionexpression.js
@@ -0,0 +1,18 @@
+//normal function statement
+function hi(){
+
+console.log('hello world !!!!!!!!');
+
+}
+
+hi();
+
+//function expression
+var bye = function(){
+
+console.log('bye people!!!!!!!!!!');
+
+}
+
+
+bye();
